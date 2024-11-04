ea4ec81468ecf68409b21730479767aa2e9c4462
Ignacio Ortego <ignacioortego110@gmail.com>
2024-11-04 17:56:40 -0300

    ej 20 corregido

--- arquitectura/practicas/practica3/ej20/ej20.s
+++ arquitectura/practicas/practica3/ej20/ej20.s
@@ -10,11 +10,11 @@ main:
   movq %rsp, %rbp
 
   # Obtener argc y argv
-  movq %rdi, %r8  # argc
-  movq %rsi, %r9  # argv
+  movq %rdi, %r12  # argc
+  movq %rsi, %r13  # argv
 
   # argc < 2
-  cmpq $2, %r8
+  cmpq $2, %r12
   jg case2
 
 case1:
@@ -24,28 +24,28 @@ case1:
   jmp end # Llama al printf y salta al final
 
 case2:
-  xor %rdx, %rdx # rdx es el valor de suma, inicializado en 0
-  movq $1, %rcx  # Inicializar el índice del bucle en 1
+  xor %r14, %r14 # r14 es el valor de suma, inicializado en 0
+  movq $1, %rbx  # Inicializar el índice del bucle en 1
 
 for_loop:
-  cmpq %r8, %rcx  # Comparar el índice con argc
+  cmpq %r12, %rbx  # Comparar el índice con argc
   jge print_result # Si el índice es mayor o igual a argc, salir del bucle
 
-  # Convertir argv[rcx] a entero y sumarlo a rdx
-  movq (%r9,%rcx,8), %rdi  # Cargar argv[rcx] en rdi. 
+  # Convertir argv[rbx] a entero y sumarlo a r14
+  movq (%r13,%rbx,8), %rdi  # Cargar argv[rbx] en rdi. 
                            # Multiplica por 8 porque las entradas de argv son punteros de 8 bytes
   call atoi
-  addq %rax, %rdx  # Sumar el resultado de atoi a rdx
-
-  incq %rcx  # Incrementar el índice
+  addq %rax, %r14  # Sumar el resultado de atoi a r14
+  incq %rbx  # Incrementar el índice
   
   jmp for_loop
 
 print_result:
   # Imprimir la cantidad de argumentos y la suma
-  movq %r8, %rsi     # Primer argumento: argc en %rsi , Segundo argumento: suma ubicada ya en %rdx
-  movq $case_2, %rdi # formato
-  xorq %rax, %rax    # cantidad de valores de punto flotante
+  movq %r12, %rsi     # Primer argumento: argc en %rsi
+  movq %r14, %rdx     # Segundo argumento: suma en %rdx
+  movq $case_2, %rdi  # formato
+  xorq %rax, %rax     # cantidad de valores de punto flotante
   call printf
 
 end:
@@ -54,26 +54,3 @@ end:
   popq %rbp
   ret
 
-
-
-# Implementación de atoi
-atoi:
-  pushq %rbp
-  movq %rsp, %rbp
-  subq $16, %rsp # Reserva espacio en la pila
-  movq %rdi, -8(%rbp) # rdi es el puntero a la cadena argv[], y lo guarda en la pila
-  movl $0, %eax # acumulador = 0
-  movl $0, %ecx # indice = 0
-atoi_loop:
-  movzbl (%rdi,%rcx,1), %edx # movzbl convierte un byte a 4 bytes, y carga el byte actual
-                             # de la cadena argv[rcx] en %edx
-  testb %dl, %dl # Comprueba que el byte no sea nulo
-  je atoi_done # Si llego al final de la cadena, termina
-  subl $48, %edx # Convierte ascii a numero
-  imull $10, %eax # Multiplica el acumulador por 10
-  addl %edx, %eax # Suma el numero al acumulador
-  incl %ecx # Incrementa el indice
-  jmp atoi_loop
-atoi_done:
-  leave
-  ret
