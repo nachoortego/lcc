e606adb57ab627cf32af0c3759f7b890d88acbcc
nachoortego <ignacioortego110@gmail.com>
2025-04-30 14:10:50 -0300

    pract 4 estructuras 2

--- estructuras2/pract4.hs
+++ estructuras2/pract4.hs
@@ -1,3 +1,6 @@
+(|||):: (a,b)->(a,b)
+x|||y = (x, y)
+
 {- 
 --1)
 --completo :: a → Int → Tree a, tal que dado un valor x de tipo a y un entero d , crea un arbol binario completo de altura d con el valor x en cada nodo.
@@ -43,6 +46,27 @@ splitBST (N l y r) x | x == y = (N l y E, r)
                                in (N l y l', r')
 
 --join :: Ord a => BST a -> BST a -> BST a
+
+--5)
+data Col = R | B
+data RBT a = Emp | N Col (RBT a) a (RBT a)
+
+-- los arbol T123 son una mierda !!!
+data T123 a = E
+    | N1 a (T123 a) (T123 a)
+    | N2 a a (T123 a) (T123 a) (T123 a)
+    | N3 a a a (T123 a) (T123 a) (T123 a) (T123 a)
+
+
+-- los arboles 123 son CLAVE ! <----
+trans :: RBT a -> T123 a
+trans Emp = E
+trans (N x B (N y R h1 h2) (N z R h3 h4)) =
+    let ((r1, r2), (r3, r4)) = (trans h1 ||| trans h2) ||| (trans h3 ||| trans h4)
+    N3 y x z r1 r2 r3 r4
+--trans N3 y x z  --raiz negra, dos hijos rojos
+--trans N2 y x    --raiz negra y hijo izq rojo / raiz negra hijo derecho rojo
+--trans N1 x      --raiz negra (INVARIANTE en este caso)
 
 --6)
 
