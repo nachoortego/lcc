6ccaf5be37bc79b7045d962c51d3496b3e4ff1e3
Ignacio Ortego <ignacioortego110@gmail.com>
2025-05-05 23:00:11 -0300

    parcial terminadoi

--- estructuras2/parcial1.hs
+++ estructuras2/parcial1.hs
@@ -4,11 +4,31 @@ key :: Treap p k -> k
 key (N _ _ k _) = k
 
 priority :: Treap p k -> p 
-key (N _ p _ _) = p
+priority (N _ p _ _) = p
 
 isTreap :: (Ord k, Ord p) => Treap p k -> Bool 
 isTreap E = True
 isTreap t = isBinSearch t && isMaxHeap t 
             where
               isBinSearch E = True
-              isBinSearch (N l p k r) = True
+              isBinSearch (N l _ k r) = k >= key l && k <= key r && isBinSearch l && isBinSearch r
+              isMaxHeap E = True
+              isMaxHeap (N l p _ r) = p >= priority r && p >= priority l
+
+rotateL (N l' p' k' (N l p k r)) = N (N l' p' k' l) p k r
+rotateR (N (N l p k r) p' k' r' ) = N l p k (N r p' k' r' )
+
+insert :: (Ord k, Ord p) => k -> p -> Treap p k -> Treap p k
+insert k p E = N E p k E
+insert k p (N l p' k' r) | k == k'  = N l p k r
+                         | k < k'   = let l' = insert k p l
+                                      in if priority l' > p' then rotateR (N l' p' k' r)
+                                                             else N l' p' k' r
+                         | k > k'   = let r' = insert k p r
+                                      in if priority r' > p' then rotateL (N l p' k' r')
+                                                             else N l p' k' r'
+
+split :: (Ord k, Ord p, Num p) => k -> Treap p k -> (Treap p k, Treap p k)
+split _ E = (E, E)
+split k t@(N _ p _ _) =  let (N l _ _ r) = insert k (p + 1) t
+                         in (l, r)
