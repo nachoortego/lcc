ea8717b332e6440400d25dc92e4f5661a07226df
Ignacio Ortego <ignacioortego110@gmail.com>
2025-06-06 17:29:09 -0300

    P3 act

---
+++ sist_operativos/P3/prod-cons-sem.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define M 5
+#define N 5
+#define SZ 8
+
+sem_t ocupados;
+sem_t libres;
+
+/*
+ * El buffer guarda punteros a enteros, los
+ * productores los consiguen con malloc() y los
+ * consumidores los liberan con free()
+ */
+int *buffer[SZ];
+
+void enviar(int *p)
+{
+  sem_wait(&libres);
+  buffer[i] = p; // apilar
+  i++;
+  sem_post(&ocupados);
+  
+	return;
+}
+
+int * recibir()
+{
+  int *v;
+
+  sem_wait(&ocupados);
+  i--; // desapilar primero
+  v = buffer[i];
+  sem_post(&libres);
+
+  return v;
+}
+
+void * prod_f(void *arg)
+{
+	int id = arg - (void*)0;
+	while (1) {
+		sleep(random() % 3);
+
+		int *p = malloc(sizeof *p);
+		*p = random() % 100;
+		printf("Productor %d: produje %p->%d\n", id, p, *p);
+		enviar(p);
+	}
+	return NULL;
+}
+
+void * cons_f(void *arg)
+{
+	int id = arg - (void*)0;
+	while (1) {
+		sleep(random() % 3);
+
+		int *p = recibir();
+		printf("Consumidor %d: obtuve %p->%d\n", id, p, *p);
+		free(p);
+	}
+	return NULL;
+}
+
+int main()
+{
+	pthread_t productores[M], consumidores[N];
+	int i;
+
+  int sem_init(&ocupados, NULL, 0);
+  int sem_init(&libres, NULL, SZ);
+
+
+	for (i = 0; i < M; i++)
+		pthread_create(&productores[i], NULL, prod_f, i + (void*)0);
+
+	for (i = 0; i < N; i++)
+		pthread_create(&consumidores[i], NULL, cons_f, i + (void*)0);
+
+	pthread_join(productores[0], NULL); /* Espera para siempre */
+	return 0;
+}
